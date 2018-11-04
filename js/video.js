d85c2e3ae44cc9ebebb551d3fe65303926c5a1d7
vand326 <42849525+vand326@users.noreply.github.com>
2018-11-04 10:03:22 -0800

    vid

--- js/video.js
+++ js/video.js
@@ -1,9 +1,9 @@
 var $pipes = $('.pipes');
 
-$pipes.on('mouseenter focus', function () {
+$pipes.on('mouseclick', function () {
   $pipes.get(0).play();
 });
 
-$polystar.on('mouseout blur', function () {
+$polystar.on('mouseout', function () {
   $pipes.get(0).pause();
 });
